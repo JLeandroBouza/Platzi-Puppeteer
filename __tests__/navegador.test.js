fb3ee6356af48b7145037c0ba202412c69922940
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-04-16 00:31:34 -0300

    Se agregar a la prueba: Recargar la página y navegar a otro sitio. Revisar la docu para verificar error en los tiempos de espera

--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -1,20 +1,31 @@
-const puppeteer = require ('puppeteer')
+const puppeteer = require('puppeteer');
 
-describe ('Mi primer test en puppeteer', () =>{
+describe('Mi primer test en puppeteer', () => {
 
-    it('Debe de abrir y cerrar el navegador', async()=>{
+    it('Debe de abrir y cerrar el navegador', async function () {
         const browser = await puppeteer.launch({
-            headless: false
-        })
-        const page = await browser.newPage ()
-        await page.goto ('https://platzi.com/')
+            headless: false,
+            defaultViewport: null
+        });
+        const page = await browser.newPage();
+        await page.goto('https://google.com.ar/');
         await page.evaluate(() => {
             return new Promise((resolve) => {
-            setTimeout(resolve, 1000);
+                setTimeout(resolve, 1000);
             });
-            });
-        await browser.close()
-    })
+        });
+        await page.waitForSelector('img');
 
-    
-})
+        //recargar la pagina
+        await page.reload();
+        await page.waitForSelector('img');
+
+        //navegar a otro sitio
+        await page.goto('https://platzi.com/');
+        await page.waitForSelector('body > main > header > div > figure > svg > g > path:nth-child(2)');
+
+        await browser.close();
+    });
+
+
+});
