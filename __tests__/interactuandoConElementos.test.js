e53e2e00eef5c5b972f316d9b7ae98742f74e55f
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-04-17 02:03:22 -0300

    Interacción con elementos, tipos de apertura del navegador, dialog

---
+++ __tests__/interactuandoConElementos.test.js
@@ -0,0 +1,55 @@
+const { Dialog } = require('puppeteer');
+const puppeteer = require('puppeteer');
+jest.setTimeout(35000);
+
+describe ('Interactuando con los elementos',() =>{
+
+    it('Debe de abrir y cerrar el navegador', async () =>{
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null
+        })
+    
+    
+    const page = await browser.newPage()
+    await page.goto('https://demo.guru99.com/test/simple_context_menu.html')
+
+    page.on('dialog', async (dialog) =>{
+        await dialog.accept()
+    })
+
+    //click derecho
+
+    await page.click('#authentication > span', {button: 'right', delay: 500})
+    await page.evaluate(() => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, 3000);
+        });
+    });
+
+    //doble click
+
+    await page.click ('#authentication > button', {clickCount: 2, delay: 500 })
+    await page.evaluate(() => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, 3000);
+        });
+    });
+
+    // Ingresar valores en un input
+
+    const page2 = await browser.newPage()
+    await page2.goto('https://devexpress.github.io/testcafe/example/')
+    await page2.type('#developer-name', 'Javier', {delay: 100 })
+    await page2.click
+    await page2.evaluate(() => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, 3000);
+        });
+    });
+
+    await browser.close ()
+
+    })
+
+})
--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -1,10 +1,14 @@
 const puppeteer = require('puppeteer');
+jest.setTimeout(35000);
 
 describe('Mi primer test en puppeteer', () => {
 
-    it('Debe de abrir y cerrar el navegador', async function () {
+    test('Debe de abrir y cerrar el navegador', async function () {
+        
         const browser = await puppeteer.launch({
             headless: false,
+            slowMo: 0,
+            devtools: false,
             defaultViewport: null
         });
         const page = await browser.newPage();
@@ -23,6 +27,16 @@ describe('Mi primer test en puppeteer', () => {
         //navegar a otro sitio
         await page.goto('https://platzi.com/');
         await page.waitForSelector('body > main > header > div > figure > svg > g > path:nth-child(2)');
+
+        //navegar hacia atras
+        await page.goBack()
+        await page.goForward()
+
+        //abrir otra pagina
+
+        const page2= await browser.newPage();
+        await page2.goto('https://www.netflix.com/')
+
 
         await browser.close();
     });
