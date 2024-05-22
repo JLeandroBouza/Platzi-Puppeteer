7b2dfced278619ee066a1302288177af3ed25f9c
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-05-21 23:06:03 -0300

    Extracción de información, manejo de tiempos

---
+++ __tests__/extrayendoinfo.test.js
@@ -0,0 +1,48 @@
+const { Dialog } = require('puppeteer');
+const puppeteer = require('puppeteer');
+jest.setTimeout(35000);
+
+describe ('Extrayendo información',() =>{
+
+    it('Extraer el titulo', async () =>{
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+        })
+
+    const page = await browser.newPage()
+    await page.goto('https://platzi.com' , {waitUntil: 'networkidle0' })
+    const titulo = await page.title()
+    const url = await page.url()
+
+// sirve para validar que el titulo o direccion sea la correcta al momento de probar. 
+    console.log('titulo',titulo)
+    console.log('url',url)
+
+    await page.waitForSelector('body > main > header > div > button')
+    const nombreBoton = await page.$eval('body > main > header > div > button', (button)=> button.textContent())
+    
+    console.log('nombreBoton', nombreBoton)
+
+// para extrar info con xpath
+
+    const [button] = await page.$x('/html/body/main/header/div/button')
+    const propiedad = await button.getProperty('textContent')
+    const texto = await propiedad.jsonValue()
+
+    console.log ('texto', texto)
+
+    // segunda forma para extraer con xpath
+
+    const texto2= await page.evaluate((name)=> name.textContent, button)
+    console.log('texto2',texto2)
+
+// contar elementos en la web
+
+    const images = await page.$$eval('img', ()=> imagenes.length)
+    console.log('images',images)
+
+    await browser.close()
+
+})
+})
