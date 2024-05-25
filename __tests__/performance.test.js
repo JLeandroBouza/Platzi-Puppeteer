be5f0f81125b472f643970265d7f3812db7bedf7
JLeandroBouza <jorge.leandrobouza@gmail.com>
2024-05-24 23:36:30 -0300

    Performance Testing y tracers

--- __tests__/performance.test.js
+++ __tests__/performance.test.js
@@ -59,4 +59,30 @@ describe ('Accesibilidad',() =>{
     
     },50000)
 
+    it('Medir la performance del page load con screenshots', async () =>{
+        
+        await page.tracing.start({path: 'profile.json', screenshot: true})
+        await page.goto('https://platzi.com')
+        await page.tracing.stop()
+        const tracing = JSON.parse(fs.readFileSync('./profile.json', 'utf8'))
+        //Filtrar el JSON
+        const traceScreenShots = tracing.traceEvents.filter(
+            (x)=>
+                x.cat === 'disabled-by-default-devtools.screenshot' &&
+                x.name === 'Screenshot' &&
+                typeof x.args !== 'undefined' &&
+                typeof x.args.snapshot !== 'undefined'
+        )
+        ///Iterar sobre este arreglo para crear las imagenes
+
+        traceScreenShots.forEach(function(snap,index){
+            fs.writeFile(`trace-screenshot-${index}.png`, snap.args.snapshot, 'base64', function(err){
+                if(err){
+                    console.log('No pude crear el archivo', err)
+                }
+            })
+        })
+    
+    },50000)
+
 })
